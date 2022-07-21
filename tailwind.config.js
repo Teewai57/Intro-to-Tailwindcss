4e19c40406a849e2ddf07e3ccf0f602d0cd787aa
Teewai57 <bobtoyoabasi@gmail.com>
2022-07-21 23:03:38 +0100

    added font weight config

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,17 @@ module.exports = {
     fontFamily: {
       sans: ['Outfit', 'sans-serif'],
     },
+    fontWeight: {
+      thin: "100",
+      extralight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      black: "900",
+    },
 
     container:{
       center:true,
